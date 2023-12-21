c62eb98b4897ff0fe519e8637b5a98a890f16b95
Manuel Schoebel <mail@manuel-schoebel.com>
2023-12-21 22:00:02 +0100

    strapi database settings improved

--- headless-strapi/config/environments/local.ts
+++ headless-strapi/config/environments/local.ts
@@ -1,4 +1,25 @@
-export default (env) => ({
-  enablePreviewUrl: "http://localhost:3000/api/draft",
-  revalidationUrl: "http://localhost:3000/api/revalidate",
-});
+import path from "path";
+
+export default (env) => {
+  const filename = path.join(
+    __dirname,
+    "..",
+    "..",
+    "..",
+    env("DATABASE_FILENAME", ".tmp/data.db")
+  );
+  return {
+    enablePreviewUrl: "http://localhost:3000/api/draft",
+    revalidationUrl: "http://localhost:3000/api/revalidate",
+    database: {
+      connection: {
+        client: "sqlite",
+        connection: {
+          filename,
+        },
+        useNullAsDefault: true,
+        acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      },
+    },
+  };
+};
--- headless-strapi/config/environments/production.ts
+++ headless-strapi/config/environments/production.ts
@@ -1,4 +1,21 @@
-export default (env) => ({
-  enablePreviewUrl: "",
-  revalidationUrl: "",
-});
+import { ConnectionString } from "connection-string";
+
+export default (env) => {
+  const dbUrl = env("DATABASE_URL", "");
+  const connectionString = new ConnectionString(dbUrl);
+  const { hostname, port, user, password, path } = connectionString;
+  const connection: any = { host: hostname, user, password, database: path[0] };
+  if (port) connection.port = port;
+  connection.ssl = env.bool("STRAPI_DB_SSL", false);
+
+  return {
+    enablePreviewUrl: "htts://www.manuel-schoebel.com/api/draft",
+    revalidationUrl: "https://www.manuel-schoebel.com/api/revalidate",
+    database: {
+      connection: {
+        client: "mysql",
+        connection: connection,
+      },
+    },
+  };
+};
