854cf4825ed4785c0f769d37fa19750d440f63d4
Manuel Schoebel <mail@manuel-schoebel.com>
2023-12-21 21:45:17 +0100

    nextjs and strapi setup optim

--- headless-nextjs/src/config/environments/local.ts
+++ headless-nextjs/src/config/environments/local.ts
@@ -1,12 +1,9 @@
 export const local = {
   strapi: {
-    url: "http://127.0.0.1:1340",
-    graphqlUrl: "http://127.0.0.1:1340/graphql",
+    url: "http://127.0.0.1:1337",
+    graphqlUrl: "http://127.0.0.1:1337/graphql",
   },
   consent: {
     cookieDomain: "localhost",
-  },
-  paypal: {
-    clientId: "",
   },
 };
