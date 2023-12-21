3a9d4e52694d42a7cf6533739ac829243555577f
Manuel Schoebel <mail@manuel-schoebel.com>
2023-12-21 22:02:16 +0100

    removed not yet finished paypal plugin

--- headless-strapi/src/plugins/paypal/README.md
+++
@@ -1,3 +0,0 @@
-# Strapi plugin paypal
-
-A quick description of paypal.
--- headless-strapi/src/plugins/paypal/server/register.ts
+++
@@ -1,7 +0,0 @@
-import { Strapi } from "@strapi/strapi";
-import graphql from "./graphql";
-
-export default ({ strapi }: { strapi: Strapi }) => {
-  // register phase
-  graphql(strapi);
-};
