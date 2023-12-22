3b9070c0bc62e74218d7c8efbfc5b5e4ff2f9dea
Manuel Schoebel <mail@manuel-schoebel.com>
2023-12-22 14:51:28 +0100

    fix codegen misconfiguration

--- headless-nextjs/codegen.js
+++ headless-nextjs/codegen.js
@@ -3,7 +3,7 @@ const generate = require("@graphql-codegen/cli").generate;
 async function doSomething() {
   await generate(
     {
-      schema: "http://127.0.0.1:1340" + "/graphql",
+      schema: "http://127.0.0.1:1337" + "/graphql",
       documents: ["./src/graphql/**/*.graphql"],
       generates: {
         [process.cwd() + "/src/graphql/generated/graphql.tsx"]: {
